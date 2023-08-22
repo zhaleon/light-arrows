5163cb55e27070a774e5fad25e1aed51c611042a
Jason Chen <chezbgone@gmail.com>
2023-08-22 11:57:35 -0400

    Slightly more interesting example with cleaner code

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 extern crate image;
 
-use image::{ImageBuffer, Rgb};
+use std::fs;
+use image::{Rgb, RgbImage};
 
-const WIDTH: u32 = 10;
-const HEIGHT: u32 = 10;
+const WIDTH: u32 = 256;
+const HEIGHT: u32 = 256;
 
 fn main() {
-    // a default (black) image containing Rgb values
-    let mut image = ImageBuffer::<Rgb<u8>, Vec<u8>>::new(WIDTH, HEIGHT);
+    let mut image = RgbImage::new(WIDTH, HEIGHT);
 
-    // set a central pixel to white
-    // image.get_pixel_mut(5, 5).data = [255, 255, 255];
-    image.get_pixel_mut(5, 5).0 = [255, 255, 255];
+    for x in 0u8..=255 {
+        for y in 0u8..=255 {
+            image.put_pixel(x.into(), y.into(), Rgb([0, x, y]));
+        }
+    }
 
     fs::create_dir_all("out").unwrap();
     image.save("out/output.png").unwrap();
